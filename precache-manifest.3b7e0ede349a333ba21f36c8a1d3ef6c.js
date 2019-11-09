dc279f6fa725a91422841b518ac21fbeee31f41f
Mofope Ojosh <mofopeojosh@gmail.com>
2019-11-09 16:36:59 +0100

    deploy

---
+++ precache-manifest.3b7e0ede349a333ba21f36c8a1d3ef6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8292eac9595f06708537",
+    "url": "/ftable-client/css/app.0c65d2a0.css"
+  },
+  {
+    "revision": "7519a16a8067a02f451c",
+    "url": "/ftable-client/css/chunk-vendors.a1c4e190.css"
+  },
+  {
+    "revision": "8d1f57a1876c98d47c1f03e9abcc1902",
+    "url": "/ftable-client/index.html"
+  },
+  {
+    "revision": "9099b4a9409737d34bf2",
+    "url": "/ftable-client/js/about.189eb40c.js"
+  },
+  {
+    "revision": "8292eac9595f06708537",
+    "url": "/ftable-client/js/app.0d692acb.js"
+  },
+  {
+    "revision": "7519a16a8067a02f451c",
+    "url": "/ftable-client/js/chunk-vendors.766daee5.js"
+  },
+  {
+    "revision": "967d19d2482e6f4600a12bf7a5e89b69",
+    "url": "/ftable-client/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ftable-client/robots.txt"
+  }
+]);
